5ff2c8823de9fe92a80e4a5fc74069fe9c020187
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-07-29 15:39:28 +0530

    Simple request and response

--- src/main/java/com/example/spring1/api/Customer.java
+++ src/main/java/com/example/spring1/api/Customer.java
@@ -1,4 +1,15 @@
 package com.example.spring1.api;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class Customer {
+    @PostMapping
+    public String saveCustomer(){
+        System.out.println("Customer saved");
+        return "Customer saved";
+    }
 }
