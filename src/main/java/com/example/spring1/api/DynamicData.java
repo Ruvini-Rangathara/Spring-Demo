3dc791f2fccfea69e1d27e4ab406036c0141f174
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-08-05 14:47:16 +0530

    Dynamic Data

---
+++ src/main/java/com/example/spring1/api/DynamicData.java
@@ -0,0 +1,22 @@
+package com.example.spring1.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dynamic")
+public class DynamicData {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> saveOrUpdate(){
+        System.out.println("save or update");
+        if(Math.random()>0.5){
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+}
--- src/main/java/com/example/spring1/config/WebRootConfig.java
+++ src/main/java/com/example/spring1/config/WebRootConfig.java
@@ -1,6 +1,8 @@
 package com.example.spring1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
