e45472cc0a3b8aa7fcb6e7762d8db298707fee89
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-08-05 15:35:24 +0530

    Add Response Entity

---
+++ src/main/java/com/example/spring1/api/JsonHandler.java
@@ -0,0 +1,19 @@
+package com.example.spring1.api;
+
+import com.example.spring1.dto.JsonData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jsonHandler")
+public class JsonHandler {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JsonData> getJsonData(){
+        System.out.println("Json Handler");
+        return new ResponseEntity<>(new JsonData("J001", "Ruvini","Panadura"), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/example/spring1/dto/JsonData.java
@@ -0,0 +1,14 @@
+package com.example.spring1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JsonData {
+    private String id;
+    private String name;
+    private String city;
+}
